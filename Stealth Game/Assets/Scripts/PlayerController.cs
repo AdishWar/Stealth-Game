8a26e0abead74743a675df0bc77e52482775f1b6
Adish War <adishwarjain767@gmail.com>
2018-03-29 00:25:02 +0530

    Smooth rotation and movement
    player now moves and turns smoothly, and angle doesn't reset to zero on no input condition...

--- Stealth Game/Assets/Scripts/PlayerController.cs
+++ Stealth Game/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    float moveSpeed = 7;
+    public float moveSpeed = 7;
+    public float smoothMoveTime = 0.1f;
+    public float turnSpeed = 8;
+
+    float angle;
+    float smoothInputMagnitude;
+    float smoothMoveVelocity;
 	
 	void Update () {
 
         Vector3 inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        float inputMagnitude = inputDirection.magnitude;
         float turnAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+ 
+        smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
+        angle = Mathf.LerpAngle(angle, turnAngle, turnSpeed * inputMagnitude  * Time.deltaTime);
 
-        transform.eulerAngles = Vector3.up * turnAngle;
-        transform.Translate(inputDirection * moveSpeed * Time.deltaTime, Space.World);
+        transform.eulerAngles = Vector3.up * angle;
+        transform.Translate(inputDirection * moveSpeed * smoothInputMagnitude * Time.deltaTime, Space.World);
 
 	}
 }
