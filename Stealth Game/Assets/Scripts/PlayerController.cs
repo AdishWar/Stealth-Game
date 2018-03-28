6c86754410af800a8680819afdb2c6f09db502a9
Adish War <adishwarjain767@gmail.com>
2018-03-28 17:31:30 +0530

    Player and movement
    added cube as player, playercontroller script and player now moves and rotates...

---
+++ Stealth Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    float moveSpeed = 7;
+	
+	void Update () {
+
+        Vector3 inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        float turnAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
+
+        transform.eulerAngles = Vector3.up * turnAngle;
+        transform.Translate(inputDirection * moveSpeed * Time.deltaTime, Space.World);
+
+	}
+}
