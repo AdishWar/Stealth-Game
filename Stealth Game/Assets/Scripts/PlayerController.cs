96c84b27dbc6adacf5e5eade7145bcd06edf0e3d
Adish War <adishwarjain767@gmail.com>
2018-03-29 14:39:20 +0530

    Added rigidody to player and obstacle
    and changed the code so that forces and rotation now affects the rigidbody rather than directly changing the rotation and position...

--- Stealth Game/Assets/Scripts/PlayerController.cs
+++ Stealth Game/Assets/Scripts/PlayerController.cs
@@ -8,21 +8,36 @@ public class PlayerController : MonoBehaviour {
     public float smoothMoveTime = 0.1f;
     public float turnSpeed = 8;
 
+    Rigidbody rb;
+
     float angle;
     float smoothInputMagnitude;
     float smoothMoveVelocity;
-	
-	void Update () {
+    Vector3 velocity;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    private void Update() {
 
         Vector3 inputDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
         float inputMagnitude = inputDirection.magnitude;
         float turnAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg;
- 
+
         smoothInputMagnitude = Mathf.SmoothDamp(smoothInputMagnitude, inputMagnitude, ref smoothMoveVelocity, smoothMoveTime);
-        angle = Mathf.LerpAngle(angle, turnAngle, turnSpeed * inputMagnitude  * Time.deltaTime);
+        angle = Mathf.LerpAngle(angle, turnAngle, turnSpeed * inputMagnitude * Time.deltaTime);
 
-        transform.eulerAngles = Vector3.up * angle;
-        transform.Translate(inputDirection * moveSpeed * smoothInputMagnitude * Time.deltaTime, Space.World);
 
-	}
-}
+        velocity = inputDirection * moveSpeed * smoothInputMagnitude;
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MoveRotation(Quaternion.Euler(Vector3.up * angle));
+        rb.MovePosition(rb.position + velocity * Time.deltaTime);  // deltaTime is equal to fixedDeltaTime when called from fixedUpdate()
+    }
+};
